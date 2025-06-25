e4e94265230ae76cdff1b3f16f3209957ee7f8d7
Jonathan Osorio <osoriogarrido7@gmail.com>
2025-06-24 20:08:03 -0500

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,12 @@ async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api');
-
+    app.enableCors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+      allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
